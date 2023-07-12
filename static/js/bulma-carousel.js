8e0ac3945d1c203085da374b7a811fe0ffe5c310
minarainbow <minarainbow@kaist.ac.kr>
2023-07-12 15:58:56 -0500

    added alt-texts and updated typos in the figures

--- static/js/bulma-carousel.js
+++ static/js/bulma-carousel.js
@@ -1808,131 +1808,131 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 
 
-var Swipe = function () {
-	function Swipe(slider) {
-		_classCallCheck(this, Swipe);
+// var Swipe = function () {
+// 	function Swipe(slider) {
+// 		_classCallCheck(this, Swipe);
 
-		this.slider = slider;
+// 		this.slider = slider;
 
-		this._supportsPassive = Object(__WEBPACK_IMPORTED_MODULE_1__utils_detect_supportsPassive__["a" /* default */])();
+// 		this._supportsPassive = Object(__WEBPACK_IMPORTED_MODULE_1__utils_detect_supportsPassive__["a" /* default */])();
 
-		this.onStartDrag = this.onStartDrag.bind(this);
-		this.onMoveDrag = this.onMoveDrag.bind(this);
-		this.onStopDrag = this.onStopDrag.bind(this);
+// 		this.onStartDrag = this.onStartDrag.bind(this);
+// 		this.onMoveDrag = this.onMoveDrag.bind(this);
+// 		this.onStopDrag = this.onStopDrag.bind(this);
 
-		this._init();
-	}
+// 		this._init();
+// 	}
 
-	_createClass(Swipe, [{
-		key: '_init',
-		value: function _init() {}
-	}, {
-		key: 'bindEvents',
-		value: function bindEvents() {
-			var _this = this;
+// 	_createClass(Swipe, [{
+// 		key: '_init',
+// 		value: function _init() {}
+// 	}, {
+// 		key: 'bindEvents',
+// 		value: function bindEvents() {
+// 			var _this = this;
 
-			this.slider.container.addEventListener('dragstart', function (e) {
-				if (!_this._supportsPassive) {
-					e.preventDefault();
-				}
-			});
-			this.slider.container.addEventListener('mousedown', this.onStartDrag);
-			this.slider.container.addEventListener('touchstart', this.onStartDrag);
+// 			// this.slider.container.addEventListener('dragstart', function (e) {
+// 			// 	if (!_this._supportsPassive) {
+// 			// 		e.preventDefault();
+// 			// 	}
+// 			// });
+// 			// this.slider.container.addEventListener('mousedown', this.onStartDrag);
+// 			// this.slider.container.addEventListener('touchstart', this.onStartDrag);
 
-			window.addEventListener('mousemove', this.onMoveDrag);
-			window.addEventListener('touchmove', this.onMoveDrag);
+// 			window.addEventListener('mousemove', this.onMoveDrag);
+// 			window.addEventListener('touchmove', this.onMoveDrag);
 
-			window.addEventListener('mouseup', this.onStopDrag);
-			window.addEventListener('touchend', this.onStopDrag);
-			window.addEventListener('touchcancel', this.onStopDrag);
-		}
-	}, {
-		key: 'unbindEvents',
-		value: function unbindEvents() {
-			var _this2 = this;
+// 			window.addEventListener('mouseup', this.onStopDrag);
+// 			window.addEventListener('touchend', this.onStopDrag);
+// 			window.addEventListener('touchcancel', this.onStopDrag);
+// 		}
+// 	}, {
+// 		key: 'unbindEvents',
+// 		value: function unbindEvents() {
+// 			var _this2 = this;
 
-			this.slider.container.removeEventListener('dragstart', function (e) {
-				if (!_this2._supportsPassive) {
-					e.preventDefault();
-				}
-			});
-			this.slider.container.removeEventListener('mousedown', this.onStartDrag);
-			this.slider.container.removeEventListener('touchstart', this.onStartDrag);
+// 			// this.slider.container.removeEventListener('dragstart', function (e) {
+// 			// 	if (!_this2._supportsPassive) {
+// 			// 		e.preventDefault();
+// 			// 	}
+// 			// });
+// 			// this.slider.container.removeEventListener('mousedown', this.onStartDrag);
+// 			// this.slider.container.removeEventListener('touchstart', this.onStartDrag);
 
-			window.removeEventListener('mousemove', this.onMoveDrag);
-			window.removeEventListener('touchmove', this.onMoveDrag);
+// 			window.removeEventListener('mousemove', this.onMoveDrag);
+// 			window.removeEventListener('touchmove', this.onMoveDrag);
 
-			window.removeEventListener('mouseup', this.onStopDrag);
-			window.removeEventListener('mouseup', this.onStopDrag);
-			window.removeEventListener('touchcancel', this.onStopDrag);
-		}
+// 			window.removeEventListener('mouseup', this.onStopDrag);
+// 			window.removeEventListener('mouseup', this.onStopDrag);
+// 			window.removeEventListener('touchcancel', this.onStopDrag);
+// 		}
 
-		/**
-   * @param {MouseEvent|TouchEvent}
-   */
+// 		/**
+//    * @param {MouseEvent|TouchEvent}
+//    */
 
-	}, {
-		key: 'onStartDrag',
-		value: function onStartDrag(e) {
-			if (e.touches) {
-				if (e.touches.length > 1) {
-					return;
-				} else {
-					e = e.touches[0];
-				}
-			}
+// 	}, {
+// 		key: 'onStartDrag',
+// 		value: function onStartDrag(e) {
+// 			if (e.touches) {
+// 				if (e.touches.length > 1) {
+// 					return;
+// 				} else {
+// 					e = e.touches[0];
+// 				}
+// 			}
 
-			this._origin = new __WEBPACK_IMPORTED_MODULE_0__utils_coordinate__["a" /* default */](e.screenX, e.screenY);
-			this.width = this.slider.wrapperWidth;
-			this.slider.transitioner.disable();
-		}
+// 			this._origin = new __WEBPACK_IMPORTED_MODULE_0__utils_coordinate__["a" /* default */](e.screenX, e.screenY);
+// 			this.width = this.slider.wrapperWidth;
+// 			this.slider.transitioner.disable();
+// 		}
 
-		/**
-   * @param {MouseEvent|TouchEvent}
-   */
+// 		/**
+//    * @param {MouseEvent|TouchEvent}
+//    */
 
-	}, {
-		key: 'onMoveDrag',
-		value: function onMoveDrag(e) {
-			if (this._origin) {
-				var point = e.touches ? e.touches[0] : e;
-				this._lastTranslate = new __WEBPACK_IMPORTED_MODULE_0__utils_coordinate__["a" /* default */](point.screenX - this._origin.x, point.screenY - this._origin.y);
-				if (e.touches) {
-					if (Math.abs(this._lastTranslate.x) > Math.abs(this._lastTranslate.y)) {
-						if (!this._supportsPassive) {
-							e.preventDefault();
-						}
-						e.stopPropagation();
-					}
-				}
-			}
-		}
+// 	}, {
+// 		key: 'onMoveDrag',
+// 		value: function onMoveDrag(e) {
+// 			if (this._origin) {
+// 				var point = e.touches ? e.touches[0] : e;
+// 				this._lastTranslate = new __WEBPACK_IMPORTED_MODULE_0__utils_coordinate__["a" /* default */](point.screenX - this._origin.x, point.screenY - this._origin.y);
+// 				if (e.touches) {
+// 					if (Math.abs(this._lastTranslate.x) > Math.abs(this._lastTranslate.y)) {
+// 						if (!this._supportsPassive) {
+// 							e.preventDefault();
+// 						}
+// 						e.stopPropagation();
+// 					}
+// 				}
+// 			}
+// 		}
 
-		/**
-   * @param {MouseEvent|TouchEvent}
-   */
+// 		/**
+//    * @param {MouseEvent|TouchEvent}
+//    */
 
-	}, {
-		key: 'onStopDrag',
-		value: function onStopDrag(e) {
-			if (this._origin && this._lastTranslate) {
-				if (Math.abs(this._lastTranslate.x) > 0.2 * this.width) {
-					if (this._lastTranslate.x < 0) {
-						this.slider.next();
-					} else {
-						this.slider.previous();
-					}
-				} else {
-					this.slider.show(true);
-				}
-			}
-			this._origin = null;
-			this._lastTranslate = null;
-		}
-	}]);
+// 	}, {
+// 		key: 'onStopDrag',
+// 		value: function onStopDrag(e) {
+// 			if (this._origin && this._lastTranslate) {
+// 				if (Math.abs(this._lastTranslate.x) > 0.2 * this.width) {
+// 					if (this._lastTranslate.x < 0) {
+// 						this.slider.next();
+// 					} else {
+// 						this.slider.previous();
+// 					}
+// 				} else {
+// 					this.slider.show(true);
+// 				}
+// 			}
+// 			this._origin = null;
+// 			this._lastTranslate = null;
+// 		}
+// 	}]);
 
-	return Swipe;
-}();
+// 	return Swipe;
+// }();
 
 /* harmony default export */ __webpack_exports__["a"] = (Swipe);
 
